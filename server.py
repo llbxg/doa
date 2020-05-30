74a890d341be7085ba626877baf4f83871634ef9
k-o-s-h <nutsuki@outlook.jp>
2020-05-30 13:16:43 +0900

    app

--- server.py
+++ server.py
@@ -9,15 +9,32 @@ app.registration('', 'GET', home)
 def about(env):
     return open_template('template.html', {'tp':'about', 'name':''})
 app.registration('about', 'GET', about)
-
+#2 code
 def code(env):
     return open_template('template.html', {'tp':'code', 'name':''})
 app.registration('code', 'GET', code)
+#3 app for pc
+def appforpc(env):
+    return open_template('template.html', {'tp':'appforpc', 'name':''})
+app.registration('appforpc', 'GET', appforpc)
+#4 fonts
+def fonts(env):
+    return open_template('template.html', {'tp':'fonts', 'name':''})
+app.registration('fonts', 'GET', fonts)
+#5 gallery
+def gallery(env):
+    return open_template('template.html', {'tp':'gallery', 'name':''})
+app.registration('gallery', 'GET', gallery)
 #auto writing
 #code-test-tube
 def testtube(env):
     return open_template('template.html', {'tp':'code', 'name':'test-tube'})
 app.registration('test-tube', 'GET', testtube)
+
+#appforpc-fish
+def fish(env):
+    return open_template('template.html', {'tp':'appforpc', 'name':'fish'})
+app.registration('fish', 'GET', fish)
 
 #auto writing
 app = MyMiddleware(app)
