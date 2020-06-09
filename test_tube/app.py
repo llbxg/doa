659e274494e6c316e3e0f6634fc4d93e183b4bbd
k-o-s-h <nutsuki@outlook.jp>
2020-06-10 00:19:47 +0900

    Add support for "405"

--- test_tube/app.py
+++ test_tube/app.py
@@ -4,12 +4,16 @@ from test_tube.route import Route, Static
 from test_tube.file  import open_template
 
 def e404(env):
-    return open_template('template.html', {'tp':'notfound', 'name':'', 'va':''})
+    return open_template('template.html', {'tp':'error', 'name':'404', 'va':''})
+
+def e405(env):
+    return open_template('template.html', {'tp':'error', 'name':'405', 'va':''})
 
 class App():
     def __init__(self):
         self.routes = []
         self.__e404 =Route(None, None, e404, status=404)
+        self.__e405 =Route(None, None, e405, status=404)
 
     def registration(self, path, method, callback):
         new_path = '^/' + path + '$'
@@ -19,7 +23,11 @@ class App():
         for route in self.routes:
             matched = re.match(route.path, path)
             if matched is not None:
-                return route
+                if route.method==method:
+                    return route
+                else:
+                    return self.__e405
+
         return self.__e404
 
     def __call__(self, env, start_response):
