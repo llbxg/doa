63355ea0e75b1e682f284f41b1bfd71d797c27e6
k-o-s-h <nutsuki@outlook.jp>
2020-06-11 13:27:55 +0900

    Fix gitlog()

--- benri/gitlog.py
+++ benri/gitlog.py
@@ -5,23 +5,29 @@ from urllib.parse import urlparse
 import datetime
 
 def gitlog():
-    dt = datetime.datetime(2020, 5, 28, 00, 00, 00)
     url = 'https://api.github.com/users/llbxg/events'
     req = urllib.request.Request(url)
     log=''
+    insect = ['d03e4bb22224d45f14bad8b4c93040e236fac097', '33aa6b729f10ca613ba76f9d92f9c54a928ef013']
     try:
         with urllib.request.urlopen(req) as res:
             body = res.read()
             events=json.loads(body)
             log_list=[]
             for e in events:
+
                 repository_id=e['repo']['id']
+
                 if repository_id == 260852860:
-                    message=e['payload']['commits'][0]['message']
-                    created_at=e['created_at'].split('T')[0]
-                    date_dt = datetime.datetime.strptime(created_at, '%Y-%m-%d')
-                    if date_dt > dt:
-                        log_list.append('<p>{} {}</p>'.format(created_at, message))
+                    commits = e['payload']['commits']
+                    if commits != []:
+                        commits = commits[0]
+                        message = commits['message']
+                        sha = commits['sha']
+                        created_at=e['created_at'].split('T')[0]
+                        if ('Revert' not in message) and (sha not in insect):
+                            log_list.append('<p>{} {}</p>'.format(created_at, message))
+
             log=''.join(log_list)
             return log
 
@@ -31,7 +37,4 @@ def gitlog():
 
     except urllib.error.URLError as err:
         print(err.reason)
-        return log
-
-    except IndexError:
         return log
