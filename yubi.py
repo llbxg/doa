e39ace493c2ce29dee45c1a6a86dcc339e412aca
k-o-s-h <nutsuki@outlook.jp>
2020-06-27 21:42:02 +0900

    Changed user_id

--- yubi.py
+++ yubi.py
@@ -20,7 +20,7 @@ person_id = domain + "active"
 key_id = domain + "active#main-key"
 followers_id = domain+"followers"
 
-user_id = 'llbxgtwo'
+user_id = 'llbxg'
 
 def active(env):
     json_data = {'@context':context, 'type':'Person', 'id':person_id, "followers":followers_id, 'name':'kosh', 'preferredUsername': user_id, 
